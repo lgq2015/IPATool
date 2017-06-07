31eccf861e3a2902e0f98bb5f500c269ed3f9da9
冷秋 <516563564@qq.com>
2017-06-07 18:48:22 +0800

    加入主程序

--- IPATool/Shared/Category/CLArguments/CLArguments+IPATool.m
+++ IPATool/Shared/Category/CLArguments/CLArguments+IPATool.m
@@ -13,6 +13,9 @@
 #import "CLArguments+CodeSign.h"
 #import "CLArguments+ZipArchive.h"
 
+#import "ITIPA.h"
+#import "ITZip.h"
+
 @implementation CLArguments (IPATool)
 
 + (instancetype)sharedInstance {
@@ -34,15 +37,37 @@
 }
 
 - (void)initMain {
-	[self setCommand:nil explain:@"Operating a package"];
+	[self setCommand:nil explain:@"Operating a package, [--key -k value] input [output]"];
 	[self setKey:@"certificate" abbr:@"c" optional:YES example:@"iPhone Developer: xxxx" explain:@"Optional, sign code if type in this key."];
 	[self setKey:@"mobileprovision" abbr:@"m" optional:YES example:@"/path/to/.mobileprovision" explain:@"Optional, sign code and copy to .app if type in this key."];
 	[self setKey:@"icon" abbr:@"i" optional:YES example:@"/path/to/image" explain:@"Optional, modify icon files in .app if type in this key."];
 	[self setKey:@"bid" abbr:nil optional:YES example:@"com.unique.xxx" explain:@"Optional, modify CFBundleIdentifier in .app and all plugin if type in this key."];
 	[self setCommand:nil task:^NSError *(CLArguments *arguments) {
+		NSString *input = [arguments fullIOPathAtIndex:0];
+		NSString *output = [arguments fullIOPathAtIndex:1];
 		
+		output = output.length > 0 ? output : input;
+		
+		ITPath *inputPath = [[ITPath alloc] initWithPath:input];
+		ITPath *outputPath = [[ITPath alloc] initWithPath:output];
+		
+		ITApp *app = [arguments main_makeApp];
+		[arguments main_opreaApp:app];
+		[arguments main_package:inputPath to:outputPath];
 		return nil;
 	}];
+}
+
+- (ITApp *)main_makeApp {
+	return nil;
+}
+
+- (void)main_opreaApp:(ITApp *)app {
+	
+}
+
+- (IPAResult *)main_package:(ITPath *)in to:(ITPath *)to {
+	return IPASucceed(nil);
 }
 
 @end
